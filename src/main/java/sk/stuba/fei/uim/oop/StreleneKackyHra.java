9c7906a5e9c93beae5dd5e372b0197bb3e314bbb
DarkoShishka <darko.siska@gmail.com>
2022-03-09 13:47:04 +0100

    Generovanie hracov

--- src/main/java/sk/stuba/fei/uim/oop/StreleneKackyHra.java
+++ src/main/java/sk/stuba/fei/uim/oop/StreleneKackyHra.java
@@ -3,16 +3,17 @@ package sk.stuba.fei.uim.oop;
 import java.util.Random;
 
 public class StreleneKackyHra {
+    private Random random;
+    private Hrac[] hraci;
 
     public StreleneKackyHra() {
-        Random random = new Random();
-        Hrac[] hraci = generujHracov(random.nextInt(6) + 2);
+        random = new Random();
+        Hrac[] hraci = generujHracov(random.nextInt(5) + 2);
         System.out.println("pocet hracov je: " + hraci.length);
     }
 
     public Hrac[] generujHracov(int pocetHracov){
-        Hrac[] poleHracov;
-        poleHracov = new Hrac[pocetHracov];
+        Hrac[] poleHracov = new Hrac[pocetHracov];
         for (int i = 0; i < pocetHracov; i++) {
             poleHracov[i] = new Hrac();
         }
